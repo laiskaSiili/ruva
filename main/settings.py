fa29649dc39677114848ef5dfbe5aa4d38de9cbb
laiskaSiili <marc.folini@gmail.com>
2020-04-06 18:21:15 +0200

    Replace postgres with sqlite db

--- main/settings.py
+++ main/settings.py
@@ -63,15 +63,22 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#          'NAME': 'ruva_db',
+#          'USER': 'ruva',
+#          'PASSWORD': 'ruva',
+#          'HOST': 'localhost',
+#          'PORT': 5432,
+#     },
+# }
+
 DATABASES = {
     'default': {
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': 'ruva_db',
-         'USER': 'ruva',
-         'PASSWORD': 'ruva',
-         'HOST': 'localhost',
-         'PORT': 5432,
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
